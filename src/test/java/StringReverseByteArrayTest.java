f374ed96d6b881349c121f376c5116d8b0c7a00d
KorolevEgor <korolevegir@gmail.com>
2022-07-08 13:51:22 +0300

    Update tests

---
+++ src/test/java/StringReverseByteArrayTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Assertions;
+
+public class StringReverseByteArrayTest {
+
+    StringReverse stringReverse = new StringReverseByteArray();
+
+    // корректная работа на пустой строке
+    @org.junit.jupiter.api.Test
+    void reverseEmptyString() {
+        String expected = "";
+        String actual = stringReverse.reverse("");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // корректная работа на строке из одного символа
+    @org.junit.jupiter.api.Test
+    void reverseOneSymbol() {
+        String expected = "*";
+        String actual = stringReverse.reverse("*");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // корректная работа на строке из двух символов
+    @org.junit.jupiter.api.Test
+    void reverseTwoSymbol() {
+        String expected = "12";
+        String actual = stringReverse.reverse("21");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // корректная работа на произвольной строке
+    @org.junit.jupiter.api.Test
+    void reverseSuccess() {
+        String expected = "0123456789";
+        String actual = stringReverse.reverse("9876543210");
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
