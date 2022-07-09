a3a6b3865c8cdae3cafca153164ea5968683a8c5
KorolevEgor <korolevegir@gmail.com>
2022-07-09 11:47:27 +0300

    init project

---
+++ src/main/java/com/korolyovegor/petList/PetListApplication.java
@@ -0,0 +1,13 @@
+package com.korolyovegor.petList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/korolyovegor/petList/PetListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korolyovegor.petList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
